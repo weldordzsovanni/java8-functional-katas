c8146de5c040c48af5ede24ffcce2e513e168872
weldordzsovanni <weldordzsovanni@gmail.com>
2017-07-02 11:48:07 +0200

    kata1

--- src/main/java/katas/Kata1.java
+++ src/main/java/katas/Kata1.java
@@ -7,6 +7,7 @@ import util.DataUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: use map() to project an array of videos into an array of {id, title}-pairs
@@ -16,7 +17,11 @@ import java.util.Map;
 public class Kata1 {
     public static List<Map> execute() {
         List<Movie> movies = DataUtil.getMovies();
-
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys"));
+        List<Map> result = movies.stream().map(m->
+                ImmutableMap.of(m.getId(), m.getTitle()))
+                .collect(Collectors.toList());
+        System.out.println(result);
+        return result;
+//        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys"));
     }
 }
